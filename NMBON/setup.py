032121555c6e234c12fbccdd5e152412c14f3068
Hunter Pearson <hpearson@NMBON.local>
2020-02-12 16:57:11 -0700

    Fixed README breaking the package

--- NMBON/setup.py
+++ NMBON/setup.py
@@ -1,20 +1,12 @@
 ﻿from setuptools import setup
 
-import os
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
-# Read the README.md into a string
-with open('..\README.md', "r", encoding="utf-8-sig") as input_file:
-    README = str(input_file.read())
-
 setup(
     name = 'NMBON',
-    version = '0.0.26',
+    version = '0.0.30',
     description = 'Boiler plate module for NMBON development',
     author = 'Hunter Pearson',
     author_email = 'hunter.pearson@state.nm.us',
     url = 'https://github.com/NMBON/nmbon_pip_module',
-    long_description = README, #Use the same README.md as github
     
     py_modules = ["nmbon"],
     package_dir = {'': 'source'},
