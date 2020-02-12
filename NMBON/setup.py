0b89b4d5d426a7b69575b8a1323af94851e99184
Hunter Pearson <hpearson@NMBON.local>
2020-02-12 16:49:15 -0700

    Linked readme to github & PyPI. Created a second README.md for uploader.

--- NMBON/setup.py
+++ NMBON/setup.py
@@ -1,13 +1,20 @@
 ﻿from setuptools import setup
 
+import os
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# Read the README.md into a string
+with open('..\README.md', "r", encoding="utf-8-sig") as input_file:
+    README = str(input_file.read())
+
 setup(
     name = 'NMBON',
-    version = '0.0.25',
-    description = 'Boiler plate for NMBON development',
+    version = '0.0.26',
+    description = 'Boiler plate module for NMBON development',
     author = 'Hunter Pearson',
     author_email = 'hunter.pearson@state.nm.us',
     url = 'https://github.com/NMBON/nmbon_pip_module',
-    long_description = 'Test!',
+    long_description = README, #Use the same README.md as github
     
     py_modules = ["nmbon"],
     package_dir = {'': 'source'},
---
+++ README.md
@@ -0,0 +1,26 @@
+# NMBON PyPI Module uploader
+
+NMBON is used as a boilerplate for NMBON development.
+Common packages will be loaded into NMBON module to wrapper dependencies.
+
+
+## Getting Started
+
+Update.bat will autoincrement the version number and upload to PyPi.
+Update.bat will also auto upgrade NMBON package after it has been updated.
+
+### Installing
+
+```
+pip install NMBON
+```
+
+### Updateing
+
+```
+pip install NMBON --upgrade
+```
+
+## Authors
+
+* **Hunter Pearson** - *Initial work* - [hpearson](https://github.com/hpearson)
