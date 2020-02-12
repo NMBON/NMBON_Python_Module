7b7dda759083baeff1976144c3695df56eb74b87
Hunter Pearson <hpearson@NMBON.local>
2020-02-12 16:27:03 -0700

    Commented on .bat files and fixed pip not updateing nmbon package

--- NMBON/setup.py
+++ NMBON/setup.py
@@ -2,7 +2,7 @@
 
 setup(
     name = 'NMBON',
-    version = '0.0.21',
+    version = '0.0.24',
     description = 'Boiler plate for NMBON development',
     author = 'Hunter Pearson',
     author_email = 'hunter.pearson@state.nm.us',
--- Update.bat
+++ Update.bat
@@ -1,13 +1,36 @@
+@echo off
 :: Update the version in python
 Python update_version.py
 :: Move into directory
 cd NMBON
+:: Create a Source Distribution
 python setup.py sdist
+:: Upload Source Distribution to PyPI (https://pypi.org/project/NMBON/)
 twine upload dist/*
+:: Move into directory
 cd src
+:: Remove temp code 'egg'
 RMDIR /S /Q NMBON.egg-info
+:: Go up a directory
 cd ..
+:: Remove the generated code
 RMDIR /S /Q dist
-pause
+
+
+:: Clear the screen
+cls
+echo Please wait 40 seconds for the updated version of NMBON to be available to pip
+: Wait for 20 seconds then continue
+TIMEOUT /T 40
+:: Clear the screen
+cls
+
+::upgrade NMBON in pip (Run multiple times to be sure)
 pip install NMBON --upgrade
+pip install NMBON --upgrade
+pip install NMBON --upgrade
+echo.
+echo.
+echo Update Complete
+echo.
 pause
